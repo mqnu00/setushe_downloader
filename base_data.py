2a56d0ce8b6dcd30a25c8d2686d14152bafed60c
mqnu00 <647983952@qq.com>
2024-01-29 21:25:32 +0800

    基础文件

--- base_data.py
+++ base_data.py
@@ -7,7 +7,7 @@ pic_bed = f'https://pic-w.{key}.com'
 # 根据标签搜索到的所有图包的链接的文件保存名称
 img_package_filename = 'img_package_list.pkl'
 # 标签
-search_name = '南宫'
+search_name = ''
 # 图片保存地址(结尾不要带斜杠)
 path = r'' 
 path = path + '\\' + search_name
